b817176834c24a798c0005fa7a997fa05bca5035
liyang <272891021@qq.com>
2026-05-13 16:03:04 +0800

    feat: add Zolin favicon and site title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,14 @@ export default defineNuxtConfig({
   compatibilityDate: '2026-05-13',
   modules: ['@nuxt/eslint'],
   css: ['~/assets/main.css'],
+  app: {
+    head: {
+      title: 'Zolin的订阅转换站',
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
+    },
+  },
   nitro: {
     preset: 'vercel',
   },
